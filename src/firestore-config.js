a9eee683931ca571de83d7e0f4d3e705a6462f8f
willieinfo <willie.estrada@regent-trg.com>
2024-11-13 07:20:57 +0800

    Add files via upload

--- src/firestore-config.js
+++ src/firestore-config.js
@@ -2,6 +2,8 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,5 +22,6 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+  const db = getFirestore(app);
 
-  export { app, analytics };
+  export { app, analytics, db };
